a4f6aec0cbb8db62e42be8b1ba4b6a33c7ce7ff0
Brian Wahungu <cepteinhawks@gmail.com>
2023-01-25 14:57:25 +0300

    done

--- charity/src/components/Authentication.js
+++ charity/src/components/Authentication.js
@@ -1,28 +1,74 @@
 import React from "react"
 function Authentication(){
-    
-    function submit(e){
-e.preventdefault()
 
-    }
-    
-    return(
-        <div>
-
-            <h1>sign up</h1>
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoggedin, setIsLoggedin] = useState(false);
+   
+    const login = (e) => {
+      e.preventDefault();
+      console.log(name, email, password);
+      const userData = {
+        name,
+        email,
+        password,
+      };
+      localStorage.setItem('token-info', JSON.stringify(userData));
+      setIsLoggedin(true);
+      setName('');
+      setEmail('');
+      setPassword('');
+    };
+   
+    const logout = () => {
+      localStorage.removeItem('token-info');
+      setIsLoggedin(false);
+    };
+   
+    return (
+      <>
+        <div style={{ textAlign: 'center' }}>
+          <h1>login</h1>
+          {!isLoggedin ? (
+            <>
+              <form action="">
+                <input
+                  type="text"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                  placeholder="Name"
+                />
+                <br>
+                <input
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  placeholder="Email"
+                />
+                </br>
+                <br>
+                <input
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  placeholder="Password"
+                />
+                </br>
+                
+                <button type="submit" onClick={login}>
+                  submit
+                </button>
+              </form>
             
-            <form onSubmit = {submit}id="form">
-                <label for="username"></label>
-                <input type="text" placeholder="username" name="username" id="name"/>
-                <label for="email"></label>
-                <input type="email" placeholder="Email" name="email" id="email"/>
-                <label for="password"></label>
-                <input type="password" placeholder="password" name="password" id="password"/>
-              
-                <button type="submit">Submit</button>
-
-                 </form>
+            </> 
+          ) : (
+            <>
+              <h1>logged in</h1>
+              <button onClickCapture={logout}>logout user</button>
+            </>
+          )}
         </div>
-    )
-}
+      </>
+)}
 export default Authentication 
